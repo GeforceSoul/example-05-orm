dc3a6640a51553fea7d5f3665c06577f4f3b3f8e
a1181207784@163.com <1999418hhh>
2019-05-21 14:19:50 +0800

    SpringMVC请求映射实验

---
+++ src/main/java/com/example/example05orm/Example05OrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.example05orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example05OrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example05OrmApplication.class, args);
+    }
+
+}
